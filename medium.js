3b9915ec2b9c18617d5de084b9955a7aca0539bf
Jérémy Brunet <dfatpunk@gmail.com>
2018-09-12 17:22:24 +0200

    Code review & Performance
    **Code Review:**
    
    - Less global variables
    - Fetches the userId of the Medium profile
    - New function Parser() added
    - New comments added
    - Prettier JSON output
    
    **Performance:**
    
    - Removed the 1-second wait() inside Scraper()
    - Runs x2.5 faster 🚀🚀🚀

--- medium.js
+++ medium.js
@@ -1,74 +1,45 @@
-/**************************************************
-Copyright 2018, Jérémy Brunet, All rights reserved.
-***************************************************/
-
-/* PREREQUISITE: HOW TO FIND THE USER ID ON MEDIUM */
-
-/*
-** 1. Go to the Medium Profile you'd like to scrap
-** 2. Open your browser console (Chrome is recommended)
-** 3. Search in the source code: 
-**
-**                window.__APOLLO_STATE__ = {"User: 
-**
-** 4. Save the serie of digits and letters assigned to "User".
-*/
-
-/* PROFILE READY FOR SCRAPING */
-
-/*
-** Medium: medium.com/@jeremybrunet
-** User_ID: 7cad54db04f8
-*/
+// Copyright 2018, Jérémy Brunet, All rights reserved.
 
 var casper = require('casper').create();
 var fs = require('fs');
 
-// Name of the Medium Profile
-var medium = "Jeremy-Brunet";
+// Set var medium with the profile's username -> https://medium.com/@jeremybrunet
+var medium = "jeremybrunet";
+var medium_id;
 
-// URL: https://medium.com/_/api/users/[USER_ID]/followers
-var urlFirst = "https://medium.com/_/api/users/7cad54db04f8/followers";
-
-// URL: https://medium.com/_/api/users/[USER_ID]/profile/stream?limit=8&to=
-var urlStart = "https://medium.com/_/api/users/7cad54db04f8/profile/stream?limit=8&to=";
-
-var urlEnd = "&source=followers";
-
-var userLimit;
-var nextURL;
-
-var user_id;
-var user;
+// Variables to store and count the followers
 var users = [];
-
-var data;
-var json;
-
-var s;
-var e;
-var total_time;
 var total_users = 0;
 
+// Create JSON to parse data
+var json;
+
+// Variables to measure running time
+var s;
+var e;
+
+// Variable to check if we have reached or not the last batch of followers
 var checkNext = true;
 
+// Function to parse JSON data
+function Parser() {
+
+    // Get page content, get rid off the 16 first characters & parse JSON data
+    var data = casper.getPageContent().slice(16);
+    json = JSON.parse(data);
+}
+
+// Function to scrap followers
 function Scraper() {
 
-    if (!checkNext) { return; }
+    if (!checkNext) { 
+        return; 
+    } else {
 
-    casper.wait(1000, function(){
-
-        // Get page content
-        data = casper.getPageContent();
-
-        // Get rid off the 16 first characters
-        data = data.slice(16);
-
-        // Parse content into JSON
-        json = JSON.parse(data);
+        Parser();
 
         // Find users & store their infos in medium.json
-        user = json.payload.references.User;
+        var user = json.payload.references.User;
 
         // How many users in this batch?
         if (Object.keys(json.payload.references).length !== 0) {
@@ -76,7 +47,7 @@ function Scraper() {
 
             // Iterate to push every user in the array
             for (var i = 0; i < objLen; i++) {
-                user_id = Object.keys(user)[i];
+                var user_id = Object.keys(user)[i];
                 users.push(user[user_id]);
             }
         }
@@ -85,14 +56,14 @@ function Scraper() {
         fs.write(medium + '.json', JSON.stringify(users, undefined, 2), 'w');
 
         // Check next user limit
-        userLimit = json.payload.paging.next.to;
+        var userLimit = json.payload.paging.next.to;
 
         if (userLimit == null) {
             checkNext = false;
         }
 
         // Build next URL to scrap
-        nextURL = urlStart + userLimit + urlEnd;
+        var nextURL = "https://medium.com/_/api/users/" + medium_id + "/profile/stream?limit=8&to=" + userLimit + "&source=followers";
 
         // Open the next URL
         casper.thenOpen(nextURL, function(){
@@ -101,8 +72,10 @@ function Scraper() {
                 total_users += objLen;
             } else {
                 console.log("Scraping Over!");
+                
+                // Save the ending time
                 e = new Date().getTime();
-                total_time = (e - s) / 1000;
+                var total_time = (e - s) / 1000;
                 if (total_time < 60) {
                     casper.echo(total_users + " followers scraped in: " + total_time + "s!","INFO");
                 } else if (total_time < 3600) {
@@ -119,10 +92,21 @@ function Scraper() {
     casper.then(Scraper);
 }
 
-casper.start(urlFirst, function() {
+casper.start("https://medium.com/@" + medium + "?format=json", function() {
+    
+    // Save the starting time
     s = new Date().getTime();
+
+    Parser();
+    
+    // Fetch username of the medium account
+    medium_id = json.payload.user.userId;
+
+    // Open the first batch of followers to scrap
+    casper.thenOpen("https://medium.com/_/api/users/" + medium_id + "/followers");
 });
 
+// Recursive scraping
 casper.then(Scraper);
 
 casper.run();
